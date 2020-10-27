c074d3283b96da0d9f387e9138ce45664310b1e0
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-27 20:00:55 -0300

    Add firebaseConfig in app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDXHP95xunTSBO0U61PlNhOLjyjXmIVXRI",
+  authDomain: "conpresp.firebaseapp.com",
+  databaseURL: "https://conpresp.firebaseio.com",
+  projectId: "conpresp",
+  storageBucket: "conpresp.appspot.com",
+  messagingSenderId: "493438146924",
+  appId: "1:493438146924:web:7b8ba5f8aacb97b114fb49",
+  measurementId: "G-RV1V75RE2Q",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+const auth = firebase.auth();
+const fs = firebase.firestore();
